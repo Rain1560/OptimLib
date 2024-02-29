8187efbea9b7a09b2ae08ff544e39991a57f7148
黄雨松 <604736670@qq.com>
2024-02-29 09:32:18 +0800

    support for ls

--- include/unconstrained/newton/LBFGS.hpp
+++ include/unconstrained/newton/LBFGS.hpp
@@ -7,23 +7,22 @@
 
 namespace optim
 {
-    template <typename fp_t,
-              template <typename> class LS =
-                  MTLineSearch>
+    template <typename fp_t>
     class LBFGS final : public BaseSolver<fp_t>
     {
+        using Storage = internal::LBFGS::Storage<fp_t>;
+
     public:
         using Problem = GradProblem<fp_t>;
         using Constant = OptimConst<fp_t>;
         using BaseSolver<fp_t>::iter;
-        // static_assert(std::is_base_of_v<>)
-        using LineSearchImp = MTLineSearch<fp_t>;
+        using LineSearchImp = LineSearch<fp_t, false>;
 
     private:
         Problem *prob;
 
     public:
-        LineSearchImp ls;
+        LineSearchImp *ls;
         int max_iter = 100; ///< max number of iterations
         fp_t xtol = 1e-6;   ///< stop if |x_{k+1} - x_k| < xtol
         fp_t ftol = 1e-6;   ///< stop if |f_{k+1} - f_k| < ftol
@@ -32,109 +31,90 @@ namespace optim
         int m = 4;          ///< number of memory
         int status;         // wether solve successfully
 
+    private:
+        OPTIM_STRONG_INLINE void
+        // assign gradient to direction
+        update_direction(LineSearchArgs<fp_t, use_prox> &arg)
+        {
+            arg.direction = -arg.cur_grad;
+        }
+
+        OPTIM_INLINE void
+        update_sy(
+            Storage &sy,
+            LineSearchArgs<fp_t, use_prox> &arg)
+        {
+            sy.s = arg.cur_x - arg.prev_x;
+            sy.y = arg.cur_grad - arg.prev_grad;
+            // compute bb_step1
+            const double sTy = BMO_MAT_DOT_PROD(sy.s, sy.y);
+            const double y_nrm2 = BMO_SQUARE_NORM(sy.y);
+            if (y_nrm2 < Constant::eps || sTy < 0)
+                OPTIM_UNLIKELY
+                { // too small to storage!
+                    arg.step = step;
+                    return;
+                }
+            arg.step = sTy / y_nrm2;
+            // force bb_step to be in [min_step, max_step]
+            arg.step = std::min(
+                std::max(ls->min_step, arg.step), ls->max_step);
+            sy.rho = 1. / sTy;
+        }
+
     public:
         explicit LBFGS(
-            Problem &prob)
+            Problem &prob,
+            LineSearchImp &ls)
         {
             this->prob = &prob;
+            this->ls = &ls;
         };
 
-        fp_t solve(Mat<fp_t> &cur_x) override
+        fp_t solve(Mat<fp_t> &x) override
         {
-            using namespace internal::LBFGS;
-            const size_t n = BMO_ROWS(cur_x),
-                         k = BMO_COLS(cur_x),
-                         nk = n * k;
-            Col<fp_t> d(nk);
-            Mat<fp_t> prev_x = cur_x,
-                      cur_g(n, k), prev_g(n, k);
-            CircularArray<Storage<fp_t>> memory(m);
-            fp_t prev_loss = prob->loss(cur_x), cur_loss,
-                 g_nrm, x_diff_nrm, f_diff,
-                 bb_step = step;
-            ls.init(this->prob, prev_loss);
-            prob->grad(cur_x, prev_g);
+            CircularArray<Storage> memory(m);
+            Storage sy;
+            LineSearchArgs<fp_t, use_prox> arg(x);
+            fp_t g_nrm, x_diff_nrm, f_diff;
+            arg.update_loss(this->prob);
+            arg.update_grad(this->prob);
+            ls->init(this->prob, arg);
+            arg.step = step;
             // make step 0
-            ls.line_search(
-                bb_step, prev_g,
-                prev_x, cur_x,
-                prev_loss, cur_loss,
-                prev_g, cur_g);
-            Storage<fp_t> sy;
-            BMO_RESIZE(cur_x, nk, 1), BMO_RESIZE(prev_x, nk, 1);
-            BMO_RESIZE(cur_g, nk, 1), BMO_RESIZE(prev_g, nk, 1);
-            sy.s = cur_x - prev_x, sy.y = cur_g - prev_g;
-            // compute bb_step and update pho
-            bb_step = BMO_DOT_PROD(sy.s, sy.y);
-            sy.rho = 1. / bb_step;
-            bb_step /= BMO_SQUARE_NORM(sy.y);
-            d = -cur_g;
-            BMO_RESIZE(cur_x, n, k), BMO_RESIZE(prev_x, n, k);
-            BMO_RESIZE(cur_g, n, k), BMO_RESIZE(prev_g, n, k);
+            update_direction(arg);
+            arg.flush();
+            ls->line_search(arg);
+            // update bb_step to arg.step
+            update_sy(sy, arg);
             memory.push_back(std::move(sy));
-            // update prev_x, prev_g, prev_loss and memory
-            prev_x = std::move(cur_x);
-            prev_g = std::move(cur_g);
-            prev_loss = cur_loss;
             // begin main loop
             for (iter = 1; iter <= max_iter; iter++)
             {
-                // compute H * d
-                lbfgs_update_direction(memory, d, bb_step);
-                // begin line search
-                bb_step = 1.;
-                ls.line_search(
-                    bb_step, d,
-                    prev_x, cur_x,
-                    prev_loss, cur_loss,
-                    prev_g, cur_g);
-                // mapping may be expired, so we need to re-map
-                BMO_RESIZE(cur_x, nk, 1), BMO_RESIZE(prev_x, nk, 1);
-                BMO_RESIZE(cur_g, nk, 1), BMO_RESIZE(prev_g, nk, 1);
-                sy.s = cur_x - prev_x, sy.y = cur_g - prev_g;
-                // compute bb_step1
-                const double sTy = BMO_MAT_DOT_PROD(sy.s, sy.y);
-                const double y_nrm2 = BMO_SQUARE_NORM(sy.y);
-                if (y_nrm2 < Constant::eps)
-                { // too small to storage!
-                    d = -cur_g;
-                    bb_step = step;
-                    goto continue_main_loop;
-                }
-                if (sTy > 0) [[likely]]
-                { // use bb step1 or step2
-                    bb_step = sTy / y_nrm2;
-                    // force bb_step to be in [min_step, max_step]
-                    bb_step = std::min(
-                        std::max(ls.min_step, bb_step), ls.max_step);
-                }
-                else [[unlikely]] // sTy == 0
-                    bb_step = step;
-                sy.rho = 1. / sTy;
-                // update -gradient to d
-                d = -cur_g;
+                update_direction(arg);
+                // compute H * -grad
+                lbfgs_update_direction(arg.step, memory, arg.direction);
+                // update prev_x, prev_g, prev_loss and memory
+                arg.step = fp_t(1), arg.flush();
+                ls->line_search(arg);
+                update_sy(sy, arg);
                 // check stop criteria
-                g_nrm = BMO_FRO_NORM(cur_g);
+                g_nrm = BMO_FRO_NORM(arg.cur_grad);
                 x_diff_nrm = BMO_FRO_NORM(sy.s); // = norm(d) * step;
-                f_diff = std::abs(cur_loss - prev_loss) /
-                         std::abs(cur_loss);
+                f_diff = std::abs(arg.cur_loss - arg.prev_loss) /
+                         std::abs(arg.cur_loss);
                 if (g_nrm < gtol && x_diff_nrm < xtol && f_diff < ftol)
                 { // check stop criteria and resize back
-                    BMO_RESIZE(cur_x, n, k);
                     status = 0;
-                    break;
+                    x = std::move(arg.cur_x);
+                    return arg.cur_loss;
                 }
-                // update prev_x, prev_g, prev_loss and memory
+                // update memory
                 memory.push_back(std::move(sy));
-            continue_main_loop:
-                BMO_RESIZE(cur_x, n, k), BMO_RESIZE(prev_x, n, k);
-                BMO_RESIZE(cur_g, n, k), BMO_RESIZE(prev_g, n, k);
-                prev_x = std::move(cur_x);
-                prev_g = std::move(cur_g);
-                prev_loss = cur_loss;
             }
             status = 1; // TODO: log
-            return cur_loss;
+            x = std::move(arg.cur_x);
+            return arg.cur_loss;
         };
     };
 }
--- include/unconstrained/newton/LBFGS.ipp
+++ include/unconstrained/newton/LBFGS.ipp
@@ -1,6 +1,4 @@
-#include "unconstrained/newton/LBFGS.hpp"
-#include "base/Recorder.hpp"
-
+#include "base/BaseSolver.hpp"
 namespace optim
 {
     namespace internal::LBFGS
@@ -8,27 +6,28 @@ namespace optim
         template <typename fp_t>
         struct Storage
         {
-            Col<fp_t> s, y;
+            Mat<fp_t> s, y;
             fp_t rho, alpha;
         };
 
         template <typename fp_t>
         void lbfgs_update_direction(
+            const fp_t bb_step,
             CircularArray<Storage<fp_t>> &memory,
-            Col<fp_t> &d, const fp_t bb_step)
+            Mat<fp_t> &d)
         // note: d = -grad at the beginning
         {
             const int m = memory.size();
             for (int i = m - 1; i >= 0; i--)
             {
                 auto &mem = memory[i];
-                mem.alpha = mem.rho * BMO_DOT_PROD(mem.s, d);
+                mem.alpha = mem.rho * BMO_MAT_DOT_PROD(mem.s, d);
                 d -= mem.alpha * mem.y;
             }
             d *= bb_step;
             for (int i = 0; i <= m - 1; i++)
             {
-                const fp_t beta = memory[i].rho * BMO_DOT_PROD(memory[i].y, d);
+                const fp_t beta = memory[i].rho * BMO_MAT_DOT_PROD(memory[i].y, d);
                 d += (memory[i].alpha - beta) * memory[i].s;
             }
         };
