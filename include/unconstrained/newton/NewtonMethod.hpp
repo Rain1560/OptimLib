59b9ab1faae55bdf3173ae8d450ad767020d6abc
黄雨松 <604736670@qq.com>
2024-04-24 15:20:00 +0800

    Move all Newton Method to one

--- include/unconstrained/newton/NewtonMethod.hpp
+++ include/unconstrained/newton/NewtonMethod.hpp
@@ -1,14 +1,15 @@
 #pragma once
-#ifndef _OPTIM_NEWTON_CG_HPP_
-#define _OPTIM_NEWTON_CG_HPP_
+#ifndef _OPTIM_NEWTON_BASE_HPP__
+#define _OPTIM_NEWTON_BASE_HPP__
 
+#include "base/BaseSolver.hpp"
 #include "line_search/More_Thuente.hpp"
 #include "functions/functions.hpp"
 
 namespace optim
 {
-    template <typename fp_t = double>
-    class NewtonCG final
+    template <typename fp_t>
+    class NewtonMethod final
         : public LSBaseSolver<fp_t>
     {
     public:
@@ -25,19 +26,17 @@ namespace optim
     public:
         int max_iter = 100;
         fp_t xtol = 1e-10;
-        fp_t ftol = 1e-10;
+        fp_t ftol = 1e-6;
         fp_t gtol = 1e-6;
-        int cg_max_iter = -1;
         int status;
 
-    public:
-        explicit NewtonCG(std::shared_ptr<Problem> prob)
+        explicit NewtonMethod(std::shared_ptr<Problem> prob)
         {
             this->prob = prob;
             this->ls = std::make_shared<MTLS<fp_t, false>>();
         }
 
-        explicit NewtonCG(
+        explicit NewtonMethod(
             std::shared_ptr<Problem> prob,
             std::shared_ptr<LineSearchImp> ls)
         {
@@ -47,38 +46,27 @@ namespace optim
 
         fp_t solve(Mat<fp_t> &x) override
         {
-            using namespace fn;
-            const int n = BMO_ROWS(x),
-                      k = BMO_COLS(x),
-                      nk = n * k;
+            const Index n = BMO_ROWS(x),
+                        k = BMO_COLS(x),
+                        nk = n * k;
             Mat<fp_t> H(nk, nk);
             LineSearchArgs<fp_t, false> arg(x);
             fp_t f_diff, x_diff_nrm, g_nrm;
-            cgParams<fp_t> cg_arg;
-            if (cg_max_iter < 0 || cg_max_iter > nk)
-                cg_arg.max_iter = nk;
-            cg_arg.tol = gtol;
             arg.update_cur_loss(prob.get());
             arg.update_cur_grad(prob.get());
             ls->init(prob, arg);
             for (iter = 1; iter <= max_iter; iter++)
             {
                 arg.flush();
-                prob->hess(arg.prev_x, H);
-                BMO_RESIZE(arg.direction, nk, 1);
-                BMO_RESIZE(arg.prev_grad, nk, 1);
-                // solve H * d = -g
-                cg(H, arg.prev_grad, arg.direction, cg_arg);
-                BMO_RESIZE(arg.direction, n, k);
-                BMO_RESIZE(arg.prev_grad, n, k);
+                prob->hess_backward(-arg.prev_grad, arg.direction);
                 arg.step = 1.;
-                arg.direction *= -1.;
                 ls->line_search(arg);
                 g_nrm = BMO_FRO_NORM(arg.cur_grad);
                 x_diff_nrm = BMO_FRO_NORM(arg.direction) * arg.step;
                 f_diff = std::abs(arg.cur_loss - arg.prev_loss) /
                          (std::abs(arg.cur_loss) + fp_t(1));
-                if ((g_nrm < gtol && x_diff_nrm < xtol && f_diff < ftol) || g_nrm < Constant::eps)
+                if ((g_nrm < gtol && x_diff_nrm < xtol && f_diff < ftol) ||
+                    g_nrm < Constant::eps)
                 { // check stop criteria
                     status = 0;
                     x = std::move(arg.cur_x);
